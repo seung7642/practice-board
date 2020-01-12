75485c6d76d7eac3c963b275cccbc2a722cb4065
SEUNGHO <seung7642@gmail.com>
2020-01-13 00:27:58 +0900

    BoardMapper.java에 SQL 매퍼 작성

--- src/main/java/com/neowiz/practice/board/application/board/dao/BoardMapper.java
+++ src/main/java/com/neowiz/practice/board/application/board/dao/BoardMapper.java
@@ -1,9 +1,16 @@
 package com.neowiz.practice.board.application.board.dao;
 
+import com.neowiz.practice.board.application.board.domain.Board;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.type.Alias;
 
-@Alias("")
+import java.util.List;
+
+@Alias("Board")
 @Mapper
 public interface BoardMapper {
+    
+    Board getArticle();
+    List<Board> getArticleList();
+    int getArticleCount();
 }
