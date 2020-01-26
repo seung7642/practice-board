b715ae4b0b5f340121db54f50b86310b171b40a5
SEUNGHO <seung7642@gmail.com>
2020-01-26 20:40:20 +0900

    Javascript .active 활성화 코드넣었는데 적용 안됨

--- src/main/resources/sql/board_tbl.sql
+++ src/main/resources/sql/board_tbl.sql
@@ -1,6 +1,11 @@
 CREATE TABLE PRACTICE_BOARD.BOARD_TBL (
     idx INT(11) AUTO_INCREMENT,
-
+    title varchar(255) NOT NULL,
+    content longtext() NOT NULL,
+    writer varchar(45) NOT NULL,
+    reg_date datetime NOT NULL, DEFAULT CURRENT_TIMESTAMP,
+    upd_date datetime NOT NULL, DEFAULT CURRENT_TIMESTAMP,
+    hits int(11) NOT NULL, DEFAULT 0,
 
     PRIMARY KEY(idx)
 )
