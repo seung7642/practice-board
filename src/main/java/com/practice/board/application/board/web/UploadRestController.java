854ca2103a372bf640e651bb94d817a45c5bc624
SEUNGHO <seung7642@gmail.com>
2020-02-08 16:38:28 +0900

    파일 업로드 작업     - UploadFileUtils 유틸리티 객체 추가     - /upload 컨트롤러에서 반환값 ResponseEntity<>()로 수정

--- src/main/java/com/practice/board/application/board/web/UploadRestController.java
+++ src/main/java/com/practice/board/application/board/web/UploadRestController.java
@@ -1,6 +1,7 @@
 package com.practice.board.application.board.web;
 
 import com.practice.board.application.board.domain.AttachFile;
+import com.practice.board.commons.utils.UploadFileUtils;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnailator;
 import net.sf.jmimemagic.*;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -33,121 +35,31 @@ public class UploadRestController {
 
     // 파일 업로드를 위해 ajax 요청을 하면 해당 컨트롤러로 들어온다.
     @PostMapping(value = "/upload")
-    public ResponseEntity<List<AttachFile>> upload(MultipartFile[] uploadFile, RedirectAttributes redirectAttributes) {
-        log.info("넘어온 첨부파일 정보 : {}", uploadFile);
-        List<AttachFile> list = new ArrayList<>();
-
-        // 년/월/일 단위로 하위 디렉터리 생성 ( 한 디렉터리에 너무 많은 파일이 생성되는 문제 방지 )
-        File uploadPath = new File(UPLOAD_DIR, getDirectory());
-        log.info("upload path: " + uploadPath);
-        if (!uploadPath.exists()) uploadPath.mkdirs();
-
-        for (MultipartFile multipartFile : uploadFile) {
-            log.info("---------------------");
-            log.info("Upload File Name: " + multipartFile.getOriginalFilename());
-            log.info("Upload File Size: " + multipartFile.getSize());
-
-            AttachFile attachFile = new AttachFile();
-
-            try {
-                String uploadFileName = getUploadFileName(multipartFile);
-                File saveFile = new File(uploadPath, uploadFileName);
-                multipartFile.transferTo(saveFile);
-                attachFile.setUuid(uploadFileName.split("_")[0]);
-
-                // 이미지 파일이라면 섬네일 이미지를 생성한다.
-                if (checkImageType(saveFile)) {
-                    attachFile.setImage(true);
-                    FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName));
-                    Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
-                    thumbnail.close();
-                }
-
-                attachFile.setFileName(multipartFile.getOriginalFilename());
-                attachFile.setUploadPath(getDirectory());
-
-                list.add(attachFile);
-            } catch (Exception e) {
-                log.error("파일첨부 작업 중 예외 발생 : {}", e.getMessage());
-            }
+    public ResponseEntity<AttachFile> upload(MultipartFile uploadFile, HttpServletRequest request) {
+        ResponseEntity<AttachFile> entity = null;
+        try {
+            entity = new ResponseEntity<>(UploadFileUtils.uploadFile(uploadFile, request), HttpStatus.CREATED);
+        } catch (Exception e) {
+            entity = new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
-
-        return new ResponseEntity<>(list, HttpStatus.OK);
+        return entity;
     }
 
     @GetMapping("/display")
     public ResponseEntity<byte[]> getFile(String fileName) {
-        log.info("fileName : {}", fileName);
+//        log.info("fileName : {}", fileName);
 
         File file = new File(UPLOAD_DIR + fileName);
         ResponseEntity<byte[]> result = null;
         HttpHeaders headers = new HttpHeaders();
-        headers.add("Content-Type", getMimeType(file));
+//        headers.add("Content-Type", getMimeType(file));
 
         try {
             result = new ResponseEntity<>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
         } catch (IOException e) {
-            log.debug(e.getMessage(), e);
+//            log.debug(e.getMessage(), e);
         }
 
         return result;
-    }
-
-    /**
-     * 한 디렉터리에 너무 많은 파일이 업로드되는 문제를 방지하기 위하여, 년/월/일에 해당하는 하위 디렉터리를 생성하기 위해
-     * 오늘 날짜를 기준으로 년/월/일 값을 구한다.
-     *
-     * @return String
-     */
-    private String getDirectory() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date();
-        String str = simpleDateFormat.format(date);
-        return str.replace("-", File.separator);
-    }
-
-    /**
-     * 실제 업로드할 때 사용될 파일명을 구한다.
-     * 중복된 파일명을 예방하기 위해 원본 파일명 앞에 랜덤한 UUID 값을 추가한다.
-     *
-     * @param multipartFile
-     * @return String
-     */
-    private String getUploadFileName(MultipartFile multipartFile) {
-        // 원본 파일명 앞에 랜덤한 UUID 값을 추가해서 중복된 파일이름 문제를 방지한다.
-        // ex) uuid_파일명
-        return UUID.randomUUID().toString() + "_" + multipartFile.getOriginalFilename();
-    }
-
-    /**
-     * 파일의 MIME 타입으로 이미지 파일인지 확인한다.
-     *
-     * @param file
-     * @return boolean
-     */
-    private boolean checkImageType(File file) {
-        String mime = getMimeType(file);
-        if (!mime.isEmpty()) return mime.contains("image");
-
-        return false;
-    }
-
-    /**
-     * 파일의 MIME 타입을 가져온다.
-     *
-     * @param file
-     * @return String
-     */
-    private String getMimeType(File file) {
-        try {
-            return Magic.getMagicMatch(file, false).getMimeType();
-        } catch (MagicParseException e) {
-            log.debug(e.getMessage(), e);
-        } catch (MagicMatchNotFoundException e) {
-            log.debug(e.getMessage(), e);
-        } catch (MagicException e) {
-            log.debug(e.getMessage(), e);
-        }
-        return "";
     }
 }
--- src/main/java/com/practice/board/commons/utils/UploadFileUtils.java
+++ src/main/java/com/practice/board/commons/utils/UploadFileUtils.java
@@ -1,5 +1,6 @@
 package com.practice.board.commons.utils;
 
+import com.practice.board.application.board.domain.AttachFile;
 import lombok.extern.slf4j.Slf4j;
 import net.coobird.thumbnailator.Thumbnailator;
 import net.sf.jmimemagic.Magic;
@@ -9,6 +10,8 @@ import net.sf.jmimemagic.MagicParseException;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -29,65 +32,66 @@ import java.util.UUID;
 @Slf4j
 public class UploadFileUtils {
 
-    private static final String UPLOAD_DIRECTORY = "/neowiz-data/board"; // TODO: 앞에 root context 경로를 추가해줘야함
+    private static final String UPLOAD_DIRECTORY = "/board"; // TODO: 앞에 root context 경로를 추가해줘야함
 
     // 파일 업로드 처리
-    public static String uploadFile(MultipartFile multipartFile) throws Exception {
-
+    public static AttachFile uploadFile(MultipartFile multipartFile, HttpServletRequest request) throws Exception {
+        AttachFile attachFile = new AttachFile();
         String originalFileName = multipartFile.getOriginalFilename(); // 파일명
-        byte[] fileData = multipartFile.getBytes();  // 파일 데이터
 
         // 1. 파일명 중복 방지 처리
-        String uuidFileName = getUuidFileName(originalFileName);
+        String uuid = getUuid();
+        String uuidFileName = getUuidFileName(uuid, originalFileName);
 
         // 2. 파일 업로드 경로 설정
-        String rootPath = getRootPath(originalFileName); // 기본경로 추출(이미지 or 일반파일)
-        String datePath = getDatePath(rootPath); // 날짜 경로 추출, 날짜 폴더 생성
+        String rootPath = getRootPath(originalFileName, request); // 기본경로 추출
+        String datePath = getDatePath(rootPath); // 날짜 경로 추출 && 날짜 폴더 생성
 
         // 3. 서버에 파일 저장
         File target = new File(rootPath + datePath, uuidFileName); // 파일 객체 생성
-        FileCopyUtils.copy(fileData, target); // 파일 객체에 파일 데이터 복사
+        multipartFile.transferTo(target);
 
         // 4. 이미지 파일인 경우 썸네일이미지 생성
         if (checkImageType(target)) {
             createThumbnail(multipartFile, rootPath + datePath, uuidFileName);
+            attachFile.setImage(true);
         }
 
-        // 5. 파일 저장 경로 치환
-        return replaceSavedFilePath(datePath, uuidFileName);
+        attachFile.setFileName(originalFileName);
+        attachFile.setUploadPath(rootPath + datePath);
+        attachFile.setUuid(uuid);
+        return attachFile;
     }
 
     // 기본 경로 추출
-    public static String getRootPath(String fileName) {
-        // TODO : root context path 구하기
-
-        return "";
+    public static String getRootPath(String fileName, HttpServletRequest request) {
+        return request.getSession().getServletContext().getRealPath(UPLOAD_DIRECTORY); // 일반파일 경로
     }
 
     /**
      * 한 디렉터리에 너무 많은 파일이 업로드되는 문제를 방지하기 위하여, 년/월/일에 해당하는 하위 디렉터리를 생성하기 위해
-     * 오늘 날짜를 기준으로 년/월/일 값을 구한다.
+     * 오늘 날짜를 기준으로 년/월/일 값을 구한다. ex) 2020 02 08
      *
-     * @param
+     * @param rootPath
      * @return String
      */
-    private static String getDatePath() {
+    private static String getDatePath(String rootPath) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
         String str = simpleDateFormat.format(date);
         str = str.replace("-", File.separator);
-        createDateDirectory(str);
+        createDateDirectory(rootPath + str);
         return str;
     }
 
     /**
-     * 년/월/일 단위로 하위 디렉터리 생성 ( 한 디렉터리에 너무 많은 파일이 생성되는 문제 방지 )
+     * 년/월/일 하위 디렉터리 생성 ( 한 디렉터리에 너무 많은 파일이 생성되는 문제 방지 )
      *
      * @param datePath
      * @return
      */
     private static void createDateDirectory(String datePath) {
-        log.info("업로드할 년/월/일 디렉터리 경로: {}" + datePath);
+//        log.info("업로드할 년/월/일 디렉터리 경로: {}" + datePath);
         File uploadPath = new File(UPLOAD_DIRECTORY, datePath);
         if (!uploadPath.exists()) uploadPath.mkdirs();
     }
@@ -105,14 +109,18 @@ public class UploadFileUtils {
         try (FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, thumbFileName))) {
             Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100);
         } catch (IOException e) {
-            log.debug(e.getMessage(), e);
+//            log.debug(e.getMessage(), e);
         }
 
         return thumbFileName;
     }
 
-    private static String getUuidFileName(String originalFileName) {
-        return UUID.randomUUID().toString() + "_" + originalFileName;
+    private static String getUuidFileName(String uuid, String originalFileName) {
+        return uuid + "_" + originalFileName;
+    }
+
+    private static String getUuid() {
+        return UUID.randomUUID().toString();
     }
 
     /**
@@ -138,11 +146,11 @@ public class UploadFileUtils {
         try {
             return Magic.getMagicMatch(file, false).getMimeType();
         } catch (MagicParseException e) {
-            log.debug(e.getMessage(), e);
+//            log.debug(e.getMessage(), e);
         } catch (MagicMatchNotFoundException e) {
-            log.debug(e.getMessage(), e);
+//            log.debug(e.getMessage(), e);
         } catch (MagicException e) {
-            log.debug(e.getMessage(), e);
+//            log.debug(e.getMessage(), e);
         }
         return "";
     }
