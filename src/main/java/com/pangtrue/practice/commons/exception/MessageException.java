767b5545bfb95c9e77b30820b1619f595ad4b765
SeungHo Lee <seung7642@neowiz.com>
2020-03-12 22:22:55 +0900

    develop 브랜치 : MessageException 예외 클래스 추가.

---
+++ src/main/java/com/pangtrue/practice/commons/exception/MessageException.java
@@ -0,0 +1,37 @@
+package com.pangtrue.practice.commons.exception;
+
+import com.pangtrue.practice.commons.constants.RETURN_TP;
+import lombok.Getter;
+
+/**
+ * User: SeungHo Lee (seung7642@gmail.com)
+ * Date: 2020. 3. 12.
+ * Time: 오후 10:10
+ */
+public class MessageException extends RuntimeException {
+
+    @Getter
+    private RETURN_TP returnTp;
+
+    @Getter
+    private int errorCode;
+
+    @Getter
+    private String errorMessage;
+
+    public MessageException(RETURN_TP returnTp) {
+        super(returnTp.getMessage());
+        this.returnTp = returnTp;
+    }
+
+    public MessageException(String errorMessage) {
+        this(RETURN_TP.FAIL, errorMessage);
+    }
+
+    public MessageException(RETURN_TP returnTp, String errorMessage) {
+        super(errorMessage);
+
+        returnTp.setMessage(errorMessage);
+        this.returnTp = returnTp;
+    }
+}
