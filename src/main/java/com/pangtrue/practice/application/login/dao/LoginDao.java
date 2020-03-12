1b90d86602cf54648978c1966fe0c53a30592d5e
SeungHo Lee <seung7642@neowiz.com>
2020-03-12 23:23:54 +0900

    develop 브랜치 : application/management 패키지 추가 - Member 클래스 생성

--- src/main/java/com/pangtrue/practice/application/login/dao/LoginDao.java
+++ src/main/java/com/pangtrue/practice/application/login/dao/LoginDao.java
@@ -2,6 +2,11 @@ package com.pangtrue.practice.application.login.dao;
 
 import org.springframework.stereotype.Repository;
 
+/**
+ * User: SeungHo Lee (seung7642@gmail.com)
+ * Date: 2020. 3. 12.
+ * Time: 오후 11:11
+ */
 @Repository
 public interface LoginDao {
 }
