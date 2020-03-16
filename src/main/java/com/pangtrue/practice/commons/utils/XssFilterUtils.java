6aff77e7b067f016f195662b7a2cf9f2c51e89d3
SeungHo Lee <seung7642@neowiz.com>
2020-03-16 10:38:56 +0900

    develop 브랜치 : 전역 설정 클래스 추가. (설정 클래스도 패키지 구조잡기)

---
+++ src/main/java/com/pangtrue/practice/commons/utils/XssFilterUtils.java
@@ -0,0 +1,11 @@
+package com.pangtrue.practice.commons.utils;
+
+/**
+ * User: SeungHo Lee (seung7642@gmail.com)
+ * Date: 2020. 3. 16.
+ * Time: 오전 10:16
+ */
+public class XssFilterUtils {
+
+    // TODO : lucy 라이브러리 사용하기.
+}
