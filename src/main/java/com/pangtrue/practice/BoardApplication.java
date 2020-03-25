0cd6a2eb8354fe3ba8c55941863fcae060153bdd
SeungHo Lee <seung7642@neowiz.com>
2020-03-25 14:23:09 +0900

    develop 브랜치 : main에 TimeZone 설정 추가.

--- src/main/java/com/pangtrue/practice/BoardApplication.java
+++ src/main/java/com/pangtrue/practice/BoardApplication.java
@@ -1,12 +1,19 @@
 package com.pangtrue.practice;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
+@Slf4j
 @SpringBootApplication
 public class BoardApplication {
 
 	public static void main(String[] args) {
+
+		// TimeZone Setting
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 
 		// Profile Setting.
 		String profile = System.getProperty("spring.profiles.active");
