9abaad142f253f9bdbb8b43400009d030584c633
Seungho Lee <seung7642@gmail.com>
2020-07-19 01:30:28 +0900

    BoardController 테스트 작성

---
+++ src/main/java/com/pangtrue/practice/configuration/persistence/JpaConfiguration.java
@@ -0,0 +1,9 @@
+package com.pangtrue.practice.configuration.persistence;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfiguration {
+}
---
+++ src/test/java/com/pangtrue/practice/application/board/web/BoardControllerTest.java
@@ -0,0 +1,113 @@
+package com.pangtrue.practice.application.board.web;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pangtrue.practice.application.board.domain.Board;
+import com.pangtrue.practice.application.board.domain.BoardRepository;
+import com.pangtrue.practice.application.board.web.dto.BoardSaveRequest;
+import com.pangtrue.practice.application.board.web.dto.BoardUpdateRequest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class BoardControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders.webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        boardRepository.deleteAll();
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void Posts_등록된다() throws Exception {
+        String title = "title";
+        String content = "content";
+        BoardSaveRequest requestDto = BoardSaveRequest.builder()
+                .title(title)
+                .content(content)
+                .author("author")
+                .build();
+
+        String url = "http://localhost:" + port + "/board/write";
+
+        mvc.perform(post(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        List<Board> all = boardRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(title);
+        assertThat(all.get(0).getContent()).isEqualTo(content);
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    public void Posts_수정된다() throws Exception {
+        Board savedPosts = boardRepository.save(Board.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        Long updateId = savedPosts.getIdx();
+        String expectedTitle = "title2";
+        String expectedContent = "content2";
+
+        BoardUpdateRequest requestDto = BoardUpdateRequest.builder()
+                .title(expectedTitle)
+                .content(expectedContent)
+                .build();
+
+        String url = "http://localhost:" + port + "/board/write/" + updateId;
+
+        mvc.perform(put(url)
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        List<Board> all = boardRepository.findAll();
+        assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
+        assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
+    }
+}
